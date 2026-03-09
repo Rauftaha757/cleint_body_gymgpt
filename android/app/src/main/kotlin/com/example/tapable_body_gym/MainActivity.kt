f74d4290eac29da1c07e2c57c6cc3d5af4e0c23c
Taha Rauf <rauftaha757@gmail.com>
2026-03-09 13:04:03 +0500

    Initialize Flutter project with essential files and configurations, including .gitignore, pubspec.yaml, and platform-specific settings for Android and iOS. Add initial assets and launch screens for both platforms.

---
+++ android/app/src/main/kotlin/com/example/tapable_body_gym/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tapable_body_gym
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'body_tap.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tapable Body',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: SafeArea(child: Center(child: BodyWidget())),
+      ),
+    );
+  }
+}
